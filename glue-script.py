df189171a23066a533e9444415cca331f8cdd8c5
Satyam <98752516+Satyam245@users.noreply.github.com>
2024-01-31 21:18:51 +0530

    add glue-script.py

---
+++ glue-script.py
@@ -0,0 +1,26 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Replace sensitive info with placeholders
+subsDf = glueContext.create_dynamic_frame.from_catalog(
+    database="your_transaction_database",
+    table_name="your_transaction_data_input_table",
+    transformation_ctx="s3_input_new"
+    )
+
+sparkSubsDf = subsDf.toDF()
+sparkSubsDf.write.mode('append').parquet('s3://your_processed_transaction_data_output_bucket/output/')
+job.commit()
